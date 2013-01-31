c2fea04eea53272cdffb6e37aee65a5606ba0f5d
Robert Schumacher <robertallenschumacher@gmail.com>
2013-01-31 12:46:11 -0600

    Added nuclear component recipes and recipes for mass fab + recycler

--- calculator.hs
+++ calculator.hs
@@ -5,7 +5,8 @@ import Data.List (maximumBy, intersperse, sort, filter)
 -- Usage
 --
 -- Change the target recipe here:
-targetRecipe = 1!NuclearReactor +++ 6!ReactorChamber
+--targetRecipe = 5!OCHeatVent +++ 2!ComponentHeatVent +++ 2!ComponentHeatExchanger
+targetRecipe = 1!Recycler
 --
 --
 
@@ -15,14 +16,34 @@ craftlist =
     , (CopperWire, 0.5!Copper +++ 1!Rubber)
     , (NuclearReactor, 3!ReactorChamber +++ 1!Generator +++ 1!AdvCircuit)
     , (AdvCircuit, 4!Redstone +++ 2!Glowstone +++ 2!Lapis +++ 1!Circuit)
-    , (Circuit, 6!CopperWire +++ 2!Redstone +++ 1!RefIron)
-    , (RefIron, 1!Iron)
+    , (Circuit, 6!CopperWire +++ 2!Redstone +++ 1!RefinedIron)
+    , (RefinedIron, 1!Iron)
     , (ReactorChamber, 4!DensePlate +++ 1!Machine)
-    , (Machine, 8!RefIron)
+    , (Machine, 8!RefinedIron)
     , (Battery, 1!CopperWire +++ 2!Redstone +++ 4!Tin)
     , (Generator, 1!Machine +++ 1!Battery +++ 1!Furnace)
     , (Furnace, 8!Cobblestone)
     , (DensePlate, 8!Copper)
+    , (OCHeatVent, 1!ReactorHeatVent +++ 2!Gold)
+    , (ReactorHeatVent, 1!HeatVent +++ 2!DensePlate)
+    , (ComponentHeatVent, 1!HeatVent +++ 4!Tin +++ 4!IronBar)
+    , (HeatVent, 4!RefinedIron +++ 4!IronBar)
+    , (ComponentHeatExchanger, 1!HeatExchanger +++ 4!Gold)
+    , (HeatExchanger, 1!DensePlate +++ 1!Circuit +++ 3!Tin)
+    , (IronBar, (6.0/16.0)!Iron)
+    , (MassFabricator, 4!Glowstone +++ 2!AdvCircuit +++ 2!AdvMachine +++ 1!LapotronCrystal)
+    , (AdvMachine, 2!AdvAlloy +++ 1!Machine +++ 2!CarbonPlate)
+    , (LapotronCrystal, 2!Circuit +++ 6!Lapis +++ 1!EnergyCrystal)
+    , (EnergyCrystal, 1!Diamond +++ 8!Redstone)
+    , (AdvAlloy, 1!MixedIngot)
+    , (CarbonPlate, 1!CarbonMesh)
+    , (CarbonMesh, 2!CarbonFiber)
+    , (CarbonFiber, 4!CoalDust)
+    , (CoalDust, 1!Coal)
+    , (MixedIngot, (3/2.0)!RefinedIron +++ (3/2.0)!Bronze +++ (3/2.0)!Tin)
+    , (Bronze, (3/2.0)!Copper +++ (1/2.0)!Tin)
+    , (Compressor, 1!Machine +++ 6!Cobblestone +++ 1!Circuit)
+    , (Recycler, 1!Compressor +++ 1!Glowstone +++ 1!RefinedIron)
     ]
 
 -- Add new blocks here
@@ -30,22 +51,50 @@ data Block = Iron
            | Copper
            | Rubber
            | Tin
-           | Circuit
-           | AdvCircuit
+           | Cobblestone
            | Glowstone
            | Redstone
+           | Gold
+           | Diamond
            | Lapis
-           | RefIron
-           | ReactorChamber
+           | Coal
+           -- Tier 1 crafts
+           | CopperWire
+           | RefinedIron
            | DensePlate
            | Machine
-           | CopperWire
+           | Furnace
+           | IronBar
+           | EnergyCrystal
+           | CoalDust
+           | Bronze
+           -- Tier 2 crafts
+           | MixedIngot
+           | Circuit
            | BatBox
            | Battery
-           | Furnace
+           | CarbonFiber
+           | CarbonMesh
+           -- Tier 3 crafts
+           | CarbonPlate
+           | AdvCircuit
+           | AdvAlloy
            | Generator
+           | LapotronCrystal
+           | Compressor
+           -- Tier 4+ crafts
+           | AdvMachine
+           | Recycler
+           | MassFabricator
+           -- Nuclear crafts
+           | ReactorChamber
            | NuclearReactor
-           | Cobblestone
+           | OCHeatVent
+           | ReactorHeatVent
+           | ComponentHeatVent
+           | HeatVent
+           | ComponentHeatExchanger
+           | HeatExchanger
            -- Meta ingredient to track process steps
            | Craft Block
            -- Stacks for calculating reduced costs
@@ -89,10 +138,12 @@ tech = fromList $ addProcess craftlist
 techtrans :: Tech -> Block -> Float -> Basket
 techtrans t b f = (findWithDefault (fromList [(b, 1.0)]) b t) *** f
 
+strip = M.filter (/= 0)
+
 reduce :: Tech -> Basket -> Basket
 reduce t b
   | b == b' = b'
-  | otherwise = reduce t b'
+  | otherwise = reduce t $ strip b'
     where b' = reduce' t b
 
 reduce' :: Tech -> Basket -> Basket
